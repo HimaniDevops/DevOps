4999228e6370998fa195e7cd87de83d105ba70c7
HimaniDevOps <shubhi30aug@gmail.com>
2024-02-23 15:30:02 +0530

    Edited

--- file1.sh
+++ file1.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 echo "Hello World"
 
-I am the Earth
-And the Earth is me
-Each blade of grass
-Each honey tree
-Each bit of mud
-And stick and stone
-Is blood and muscle
-skin and bone
-
-
